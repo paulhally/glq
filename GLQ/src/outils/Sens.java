387d56022bce55f5006e0e468683b4a13bb023ea
d20905371 <d20905371@FORBIN-216-19.salsa.univ-amu.fr>
2015-02-23 13:58:26 +0100

    javadoc Sens

--- GLQ/src/outils/Sens.java
+++ GLQ/src/outils/Sens.java
@@ -1,16 +1,45 @@
 package outils;
 
+/**
+ * Sens est un type énuméré 
+ *
+ * 
+ * Il existe trois types d'état : 
+ * Montée
+ * Descente
+ * Indéfini
+ * 
+ *
+ */
+
 public enum Sens 
 {
+	/**
+	 * Enumération des états
+	 * 
+	 */
 	MONTEE("^"), DESCENTE("v"), INDEFINI("-");
 	
+	/**
+	 * Symbole du sens
+	 */
 	private String symbole;
 	
+	
+	/**
+	 * Constructeur Sens
+	 * @param symbole
+	 * 				Le symbole de l'état
+	 */
 	private Sens(String symbole)
 	{
 		this.symbole = symbole;
 	}
 	
+	/**
+	 * Retourne la chaine de charatère correspondante au symbole
+	 * @return symbole
+	 */
 	@Override
 	public String toString()
 	{
